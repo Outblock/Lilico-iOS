ad64357a451aef0ea3b06d3b7e564f2826dcdcdd
lmcmz <lmcmze@gmail.com>
2022-08-30 09:27:44 +1000

    Update walletconnect icon

--- Lilico/Services/Manager/WalletConnect/WalletConnectManager.swift
+++ Lilico/Services/Manager/WalletConnect/WalletConnectManager.swift
@@ -37,7 +37,7 @@ class WalletConnectManager: ObservableObject {
             name: "Lilico",
             description: "A crypto wallet on Flow built for Explorers, Collectors and Gamers",
             url: "https://link.lilico.app/wc",
-            icons: ["https://lilico.app/logo-no-padding.png"])
+            icons: ["https://lilico.app/logo.png"])
         Sign.configure(metadata: metadata, projectId: "29b38ec12be4bd19bf03d7ccef29aaa6", socketFactory: SocketFactory())
         reloadActiveSessions()
         setUpAuthSubscribing()
