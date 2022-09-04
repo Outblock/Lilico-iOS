d47d540309b9d8fbb58f42c9a83514ef43da6a88
lmcmz <lmcmze@gmail.com>
2022-09-05 00:16:23 +1000

    Update NFTEmptyView

--- Lilico/Modules/NFT/NFTEmptyView.swift
+++ Lilico/Modules/NFT/NFTEmptyView.swift
@@ -6,37 +6,50 @@
 //
 
 import SwiftUI
+import RiveRuntime
 
 struct NFTEmptyView: View {
     var body: some View {
         ZStack {
-            Image("nft_empty_bg")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-
-            VStack {
-                Text("nft_empty".localized)
-                    .font(.LL.mindTitle)
-                    .foregroundColor(.LL.Neutrals.neutrals3)
-                    .padding(2)
-                Text("nft_empty_discovery".localized)
-                    .font(.LL.body)
-                    .foregroundColor(.LL.Neutrals.neutrals8)
-                Spacer()
-                    .frame(height: 18)
-                Button {} label: {
-                    Text("get_new_nft".localized)
-                        .foregroundColor(.LL.Primary.salmonPrimary)
-                }
-                .padding(.vertical, 10)
-                .padding(.horizontal, 44)
-                .background(Color.LL.Primary.salmonPrimary.opacity(0.08))
-                .cornerRadius(12)
-                .clipped()
-            }
-            .background(Color.clear)
+//            Image("nft_empty_bg")
+//                .resizable()
+//                .aspectRatio(contentMode: .fill)
+            
+            Color.LL.background
+                .ignoresSafeArea()
+            
+            RiveViewModel(fileName: "shapes").view()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .ignoresSafeArea()
+                .blur(radius: 30)
+                .blendMode(.hardLight)
+            
+            Image("Spline")
+                .blur(radius: 50)
+                .offset(x: 200, y: 100)
+            
+//            VStack {
+//                Text("nft_empty".localized)
+//                    .font(.LL.mindTitle)
+//                    .fontWeight(.bold)
+//                    .foregroundColor(.LL.Neutrals.neutrals3)
+//                    .padding(2)
+//                Text("nft_empty_discovery".localized)
+//                    .font(.LL.body)
+//                    .foregroundColor(.LL.Neutrals.neutrals8)
+//                Spacer()
+//                    .frame(height: 18)
+//                Button {} label: {
+//                    Text("get_new_nft".localized)
+//                        .foregroundColor(.LL.Primary.salmonPrimary)
+//                }
+//                .padding(.vertical, 10)
+//                .padding(.horizontal, 44)
+//                .background(Color.LL.Primary.salmonPrimary.opacity(0.08))
+//                .cornerRadius(12)
+//            }
         }
-        .ignoresSafeArea()
+        .clipped()
     }
 }
 
